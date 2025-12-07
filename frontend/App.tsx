76ca7006f0273a4d76bee14b5c2691ed5d966e43
Jason <jwu0592@gmail.com>
2025-12-07 18:30:28 -0500

    refactor: update import statements

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider, RouteRenderer, RouteConfig } from "./lib/router";
 import Layout from "./app/layout";
 import HomePage from "./app/page";
 import MusicPage from "./app/music/page";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 const routes: RouteConfig = {
   "/": HomePage,
   "/music": MusicPage,
